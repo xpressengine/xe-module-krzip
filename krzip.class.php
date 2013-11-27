a297a1a43ff1cf0c36dd3094de2c56d03383a6c3
khongchi <sungbum00@gmail.com>
2013-11-27 14:59:54 +0900

    NOISSUE, krzip 모듈 bootstrap 적용 및 신규모듈체계로 변경

--- krzip.class.php
+++ krzip.class.php
@@ -1,62 +1,62 @@
 <?php
 /* Copyright (C) NAVER <http://www.navercorp.com> */
 
-    /**
-     * @class  krzip
-     * @author NAVER (developers@xpressengine.com)
-     * @brief Super class of krzip, which is a zip code search module
-     **/
+	/**
+	 * @class  krzip
+	 * @author NAVER (developers@xpressengine.com)
+	 * @brief Super class of krzip, which is a zip code search module
+	 **/
 
-    class krzip extends ModuleObject {
+	class krzip extends ModuleObject {
 
-        var $hostname = 'kr.zip.xpressengine.com';
-        var $port = 80;
-        var $query = '/server.php';
-        
-        /**
-         * @brief Implement if additional tasks are necessary when installing
-         **/
-        function moduleInstall() {
-            return new Object();
-        }
+		var $hostname = 'kr.zip.xpressengine.com';
+		var $port = 80;
+		var $query = '/server.php';
+		
+		/**
+		 * @brief Implement if additional tasks are necessary when installing
+		 **/
+		function moduleInstall() {
+			return new Object();
+		}
 
-        /**
-         * @brief a method to check if successfully installed
-         **/
-        function checkUpdate() {
-        	
-        	$oModuleModel = &getModel('module');
-        	$config = $oModuleModel->getModuleConfig('krzip');
-        	if($config->krzip_server_hostname == 'kr.zip.zeroboard.com') return true;
-        	
-            return false;
-        }
+		/**
+		 * @brief a method to check if successfully installed
+		 **/
+		function checkUpdate() {
+			
+			$oModuleModel = &getModel('module');
+			$config = $oModuleModel->getModuleConfig('krzip');
+			if($config->krzip_server_hostname == 'kr.zip.zeroboard.com') return true;
+			
+			return false;
+		}
 
-        /**
-         * @brief Execute update
-         **/
-        function moduleUpdate() {
-        	
-        	$oModuleModel = &getModel('module');
-        	$config = $oModuleModel->getModuleConfig('krzip');
-        	if($config->krzip_server_hostname == 'kr.zip.zeroboard.com')
-        	{
-        		$config->krzip_server_hostname = $this->hostname;
-        		$config->krzip_server_port = $this->port;
-        		$config->krzip_server_query = $this->query;
-        		
-        		// Insert by creating the module Controller object
-        		$oModuleController = &getController('module');
-        		$output = $oModuleController->insertModuleConfig('krzip',$config);
-        	}
-        	
-            return new Object(0, 'success_updated');
-        }
+		/**
+		 * @brief Execute update
+		 **/
+		function moduleUpdate() {
+			
+			$oModuleModel = &getModel('module');
+			$config = $oModuleModel->getModuleConfig('krzip');
+			if($config->krzip_server_hostname == 'kr.zip.zeroboard.com')
+			{
+				$config->krzip_server_hostname = $this->hostname;
+				$config->krzip_server_port = $this->port;
+				$config->krzip_server_query = $this->query;
+				
+				// Insert by creating the module Controller object
+				$oModuleController = &getController('module');
+				$output = $oModuleController->insertModuleConfig('krzip',$config);
+			}
+			
+			return new Object(0, 'success_updated');
+		}
 
-        /**
-         * @brief Re-generate the cache file
-         **/
-        function recompileCache() {
-        }
-    }
+		/**
+		 * @brief Re-generate the cache file
+		 **/
+		function recompileCache() {
+		}
+	}
 ?>
