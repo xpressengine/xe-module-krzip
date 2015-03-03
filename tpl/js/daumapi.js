7fc9933edd1e2f72510a3bcde36f45746b338b9a
Eunsoo Lee <izuzero@gmail.com>
2015-03-03 16:58:00 +0900

    REVERT 사용자가 주소를 직접 수정할 수 없도록 수정 (2)

--- tpl/js/daumapi.js
+++ tpl/js/daumapi.js
@@ -53,11 +53,11 @@
 
 				/* 도로명 주소 저장 */
 				var roadAddr = (response.userSelectedType === "R" ? fullAddr : response.roadAddress);
-				ui.roadAddress.prop("disabled", !!roadAddr).val(roadAddr).trigger("change");
+				ui.roadAddress.val(roadAddr).trigger("change");
 
 				/* 지번 주소 저장 */
 				var jibunAddr = (response.userSelectedType === "R" ? response.jibunAddress : fullAddr);
-				ui.jibunAddress.prop("disabled", !!jibunAddr).val(jibunAddr ? "(" + jibunAddr + ")" : jibunAddr).trigger("change");
+				ui.jibunAddress.val(jibunAddr ? "(" + jibunAddr + ")" : jibunAddr).trigger("change");
 
 				/* 부가 주소 저장 */
 				ui.extraAddress.val(extraAddr).trigger("change");
@@ -93,9 +93,13 @@
 		}
 
 		/* 검색 이벤트 등록 */
-		ui.search.on("click", function (e) {
-			krzip.open();
-		});
+		key = ["postcode", "roadAddress", "jibunAddress", "extraAddress", "search"];
+		for(i = 0; i < key.length; i++) {
+			val = key[i];
+			ui[val].on("click", function (e) {
+				krzip.open();
+			});
+		}
 	};
 })(jQuery);
 
